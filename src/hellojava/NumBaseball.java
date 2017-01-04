7bcd24fc40eb49559ac288619cdce4a812b749f5
seulgi_is_love <wheejuni@gmail.com>
2017-01-04 23:36:24 +0900

    -strike count는 쉽게 구현함. ball이 문제..

--- src/hellojava/NumBaseball.java
+++ src/hellojava/NumBaseball.java
@@ -18,20 +18,94 @@
 예시는 아래와 같습니다.*/
 
 package hellojava;
+import java.util.*;
+
 
 class makearray{
+	
+	int [] throwball(){
+		Random randomize=new Random();
+		
+		int ballcontrol[]=new int [3];
+		for (int i=0;i<ballcontrol.length;i++){
+			ballcontrol[i] = randomize.nextInt(9);
+		}
+		return ballcontrol;	
+	}
+}
+
+class userball{
+	
+	int [] userthrows(){
+		
+				
+		int userball[] = new int [3];
+		Scanner makeinput=new Scanner(System.in);
+		for (int z=0;z<userball.length;z++){
+			System.out.println((z+1)+"번째 공을 던지세요!");
+			userball[z] = makeinput.nextInt();			
+		}
+		return userball;
+		
+		
+	}
 	
 }
 
 class umpire{
-	
+	int strike_ball(int []a, int []b){
+		int strikecount = 0;
+		
+		int computerball[] = new int [3];
+		int userthrewball[] = new int [3];
+		
+		computerball = a;
+		userthrewball = b;
+		
+		for(int n=0;n<computerball.length;n++){
+			
+			if (computerball[n] == userthrewball[n]){
+				strikecount += 1;
+			}
+		}
+		
+		
+		
+		return strikecount;
+	}
 }
+
 
 public class NumBaseball {
 	
 	public static void main (String args[]){
 		
+		int [] experimental = new int [3];
+		int [] userex = new int [3];
+		int strikecall=0;
 		
+		
+		makearray printcomputerball=new makearray();
+		experimental = printcomputerball.throwball();
+		
+		for(int p=0;p<experimental.length;p++){
+			System.out.println(experimental[p]);
+		}
+		
+		userball printuserball=new userball();
+		userex = printuserball.userthrows();
+		
+		for(int q=0;q<userex.length;q++){
+			System.out.println(userex[q]);
+		}
+		
+		umpire ballpanjeong=new umpire();
+		strikecall = ballpanjeong.strike_ball(experimental, userex);
+		if (strikecall == 3){
+			System.out.println("삼진 아웃!");
+			
+		}
+		else {System.out.println("스뚜라익! 현재 스트라익 카운트는 "+strikecall+" 입니다.");}
 		
 	}
 
